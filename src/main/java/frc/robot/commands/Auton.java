c45fd203756421011dec497425fb02d3feac81f8
alexandra <alexandra.ding.mm@gmail.com>
2024-10-24 20:22:53 -0400

    Initial commit

---
+++ src/main/java/frc/robot/commands/Auton.java
@@ -0,0 +1,13 @@
+package frc.robot.commands;
+import frc.robot.subsystems.XRPDrivetrain;
+import edu.wpi.first.wpilibj2.command.*;
+import 
+
+public class Auton {
+    public static Command basicAuton(){
+        var auton = Commands.sequence(
+            
+        );
+    }
+
+}
